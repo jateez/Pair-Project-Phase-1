462c6589d6430872cefab8de4bc52ce56b45ff1b
Arya Wibowo Jati <94813894+jateez@users.noreply.github.com>
2024-07-18 14:41:36 +0700

    feat: add styling for register login profile

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,5 @@ const router = require("express").Router();
 router.get("/profile", UserController.profile);
 router.get("/profile/add", UserController.showInsertProfile);
 router.post("/profile/add", UserController.insertProfile);
-
+router.get("/communities", UserController.communities)
 module.exports = router;
