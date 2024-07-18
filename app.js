a0450052a8aeb8ec304a92745f921970204719a1
Adityasputra <aditsputra.cv@gmail.com>
2024-07-18 14:42:07 +0700

    View1

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const session = require('express-session');
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public/styles'));
+app.use(express.static('public/assets'));
 // TRY
 // app.use(express.json())
 
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,25 +1,27 @@
 class Controller {
   static async home(req, res) {
     try {
-      res.send(req.session.userId);
+      // res.send(req.session.userId);
+      res.render('login1');
     } catch (error) {
       res.send(error);
     }
   }
-  // static async home() {
-  //   try {
-  //     res.send("Home");
-  //   } catch (error) {
-  //     res.send(error);
-  //   }
-  // }
-  // static async home() {
-  //   try {
-  //     res.send("Home");
-  //   } catch (error) {
-  //     res.send(error);
-  //   }
-  // }
+  static async ifLogin(req, res) {
+    try {
+
+      res.send("postHome");
+    } catch (error) {
+      res.send(error);
+    }
+  }
+  static async home() {
+    try {
+      res.send("Home");
+    } catch (error) {
+      res.send(error);
+    }
+  }
 }
 
 module.exports = Controller;
